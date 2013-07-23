e36573e12f8d441d7f35988993200e7bd09dda85
Beryl <beryl.schragger@gmail.com>
2013-07-23 09:55:13 -0400

    Change names to reflect different subway stations.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 n_line = ['Times Square',
           '34th',
-          '28th',
-          '23rd',
+          '28th and Broadway',
+          '23rd and Broadway',
           'Union Square',
-          '8th'];
-l_line = ['8th',
+          '8th St'];
+l_line = ['8th Ave',
           '6th',
           'Union Square',
           '3rd',
           '1st'];
 six_line = ['Grand Central',
           '33rd',
-          '28th',
-          '23rd',
+          '28th and Park',
+          '23rd and Park',
           'Union Square',
           'Astor Place'];
 
