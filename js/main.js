8e84f6ce012c1d97c8d5421887c4706ae85a0c02
Beryl <beryl.schragger@gmail.com>
2013-07-23 09:50:16 -0400

    Create files, add basic html and arrays for subway lines.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+n_line = ['Times Square',
+          '34th',
+          '28th',
+          '23rd',
+          'Union Square',
+          '8th'];
+l_line = ['8th',
+          '6th',
+          'Union Square',
+          '3rd',
+          '1st'];
+six_line = ['Grand Central',
+          '33rd',
+          '28th',
+          '23rd',
+          'Union Square',
+          'Astor Place'];
